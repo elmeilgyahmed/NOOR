b498c050557bb23a81c660cd4c6f7e2255055bc1
Ahmed Kamal El Melegy <50087016+elmeilgyahmed@users.noreply.github.com>
2024-01-31 20:13:32 +0200

    TranscribeSocket.java

--- speaking-with-NOOR/04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
+++ speaking-with-NOOR/04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
@@ -39,6 +39,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
