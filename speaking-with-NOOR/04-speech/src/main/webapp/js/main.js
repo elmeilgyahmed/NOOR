7ba472571d1c3b51c794fbd8e156406dbc5f2178
AhmedEidd99 <158228305+AhmedEidd99@users.noreply.github.com>
2024-02-01 03:48:30 +0200

    main.js

--- speaking-with-NOOR/04-speech/src/main/webapp/js/main.js
+++ speaking-with-NOOR/04-speech/src/main/webapp/js/main.js
@@ -275,13 +275,13 @@
     };
     transcript.el.appendChild(transcript.current);
     
-    
+    /*
     var response = {
       el: document.getElementById('noor_response').childNodes[0],
       current: document.createElement('div')
     };
     response.el.appendChild(response.current);
-    
+    */
 
     
     /**
