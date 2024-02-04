30a1cec986b7879ecfdbeb2567929111e492e910
Ahmed Kamal El Melegy <50087016+elmeilgyahmed@users.noreply.github.com>
2024-02-04 03:14:21 +0200

    TranscribeSocket.java

--- speaking-with-NOOR/04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
+++ speaking-with-NOOR/04-speech/src/main/java/com/example/flexible/speak/TranscribeSocket.java
@@ -134,7 +134,7 @@ public class TranscribeSocket extends WebSocketAdapter
         }
     }
 //===============================================================================================
-        private  void sendAudioToClient(byte[] audioData) {
+        public  void sendAudioToClient(byte[] audioData) {
     if (getSession() != null && getSession().isOpen()) {
         try {
             getSession().getRemote().sendBytesByFuture(java.nio.ByteBuffer.wrap(audioData));
